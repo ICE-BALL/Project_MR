9ee83bb2790ce1e63e538a601e8d9e80b896f096
ICE-BALL <92620792+ICE-BALL@users.noreply.github.com>
2023-12-17 15:24:23 +0900

    MMORPG - Project_MR
    Unity MMORPG Project MR -
    Mystic Realms
    
    Fixed an error that caused players to attack twice and lose double the opponent's health.

--- MMO/MMO_Client/Assets/Script/Contant/Data/Player/PlayerStat.cs
+++ MMO/MMO_Client/Assets/Script/Contant/Data/Player/PlayerStat.cs
@@ -7,4 +7,50 @@ public class PlayerStat : Stat
     protected int _playerId;
 
     public int PlayerId { get { return _playerId; } set { _playerId = value; } }
+
+    LayerMask _attackMask;
+    float _attackRange = 1.5f;
+
+    private void Start()
+    {
+        _attackMask = LayerMask.GetMask("Creature") | LayerMask.GetMask("Player");
+    }
+
+    public void OnHit()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position + Vector3.up, transform.TransformDirection(Vector3.forward), out hit, _attackRange, _attackMask))
+        {
+            if (gameObject.transform.tag == "Player")
+            {
+                if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Player"))
+                    hit.collider.gameObject.GetComponent<PlayerStat>().HitEvent(transform.gameObject);
+                else if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Creature"))
+                    hit.collider.gameObject.GetComponent<Creature>().HitEvent(transform.gameObject);
+            }
+        }
+    }
+
+    public void HitEvent(GameObject obj)
+    {
+        if (obj.gameObject.name == "MyPlayer")
+        {
+            PlayerStat stat = gameObject.GetComponent<PlayerStat>();
+            stat.Hp -= obj.GetComponent<Stat>().Attack;
+            Data data = new Data();
+            data.MaxHp = stat.MaxHp;
+            data.Attack = stat.Attack;
+            data.AttackSpeed = stat.AttackSpeed;
+            data.Hp = stat.Hp;
+            data.MaxMp = stat.MaxMp;
+            data.Mp = stat.Mp;
+            data.Speed = stat.Speed;
+            data.PlayerId = stat.PlayerId;
+            data.Map_Zone = Managers.Game.Map_Zone;
+            NetworkManager._session.Send(data.Write());
+        }
+
+
+    }
 }
--- MMO/MMO_Client/Assets/Script/Network/Player/Player.cs
+++ MMO/MMO_Client/Assets/Script/Network/Player/Player.cs
@@ -6,15 +6,12 @@ public class Player : MonoBehaviour
 {
     public int PlayerId;
     int _state = 0;
-    LayerMask _attackMask;
-    float _attackRange = 1.5f;
-
     public Animator _anim;
 
     void Start()
     {
         _anim = GetComponent<Animator>();
-        _attackMask = LayerMask.GetMask("Creature") | LayerMask.GetMask("Player");
+        
     }
 
     void Update()
@@ -42,34 +39,5 @@ public class Player : MonoBehaviour
     {
         _state = StateSpeed;
     }
-
-    public void OnHit()
-    {
-        RaycastHit hit;
-
-        if (Physics.Raycast(transform.position + Vector3.up, transform.TransformDirection(Vector3.forward), out hit, _attackRange, _attackMask))
-        {
-            if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Player"))
-                hit.collider.gameObject.GetComponent<Player>().HitEvent(transform.gameObject);
-            else if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Creature"))
-                hit.collider.gameObject.GetComponent<Creature>().HitEvent(transform.gameObject);
-        }
-    }
-
-    public void HitEvent(GameObject obj)
-    {
-        PlayerStat stat = gameObject.GetComponent<PlayerStat>();
-        stat.Hp -= obj.GetComponent<Stat>().Attack;
-        Data data = new Data();
-        data.MaxHp = stat.MaxHp;
-        data.Attack = stat.Attack;
-        data.AttackSpeed = stat.AttackSpeed;
-        data.Hp = stat.Hp;
-        data.MaxMp = stat.MaxMp;
-        data.Mp = stat.Mp;
-        data.Speed = stat.Speed;
-        data.PlayerId = stat.PlayerId;
-        data.Map_Zone = Managers.Game.Map_Zone;
-        NetworkManager._session.Send(data.Write());
-    }
+    
 }
